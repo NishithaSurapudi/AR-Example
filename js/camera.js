cd3ae0bfff63f6ca1c4d73638eeae81ff48e423b
Nishitha <nishithasurapudi@gmail.com>
2020-07-10 10:34:05 +0530

    ll

--- js/camera.js
+++ js/camera.js
@@ -67,17 +67,11 @@ AFRAME.registerComponent('pinch-scale', {
 
         if (gltfModel.object3D.scale.x<=this.maxvalue ) {
             gltfModel.object3D.scale.x = this.scaleFactor * this.initialScale.x;
-            gltfModel.object3D.scale.y = this.scaleFactor * this.initialScale.y;
-            gltfModel.object3D.scale.z = this.scaleFactor * this.initialScale.z;
        
         if (gltfModel.object3D.scale.y<=this.maxvalue ) {
-            gltfModel.object3D.scale.x = this.scaleFactor * this.initialScale.x;
             gltfModel.object3D.scale.y = this.scaleFactor * this.initialScale.y;
-            gltfModel.object3D.scale.z = this.scaleFactor * this.initialScale.z;
         }
         if (gltfModel.object3D.scale.z<=this.maxvalue ) {
-            gltfModel.object3D.scale.x = this.scaleFactor * this.initialScale.x;
-            gltfModel.object3D.scale.y = this.scaleFactor * this.initialScale.y;
             gltfModel.object3D.scale.z = this.scaleFactor * this.initialScale.z;
         }
     }
