1f72aeb700884a1bdca4ec1772c7c0636c51f9a9
Nishitha <nishithasurapudi@gmail.com>
2020-07-13 18:14:10 +0530

    changed scale completely

--- js/camera.js
+++ js/camera.js
@@ -37,7 +37,7 @@ AFRAME.registerComponent('tap-place', {
 AFRAME.registerComponent('pinch-scale', {
     schema: {
         min: { default: 0.5 },
-        max: { default: 2 }
+        max: { default: 2.5 }
     },
 
     init: function () {
@@ -58,27 +58,27 @@ AFRAME.registerComponent('pinch-scale', {
         if (isplaced) {
             gltfModel = document.getElementById('modelID1');
         }
-        if (gltfModel.object3D.scale.x < 2) {
+        if (gltfModel.object3D.scale.x <=2.5) {
             gltfModel.object3D.scale.x = this.scaleFactor * this.initialScale.x;
             gltfModel.object3D.scale.y = this.scaleFactor * this.initialScale.y;
             gltfModel.object3D.scale.z = this.scaleFactor * this.initialScale.z;
         }
-        if (gltfModel.object3D.scale.y < 2) {
+        if (gltfModel.object3D.scale.y <=2.5) {
             gltfModel.object3D.scale.x = this.scaleFactor * this.initialScale.x;
             gltfModel.object3D.scale.y = this.scaleFactor * this.initialScale.y;
             gltfModel.object3D.scale.z = this.scaleFactor * this.initialScale.z;
         }
 
-        if (gltfModel.object3D.scale.z < 2) {
+        if (gltfModel.object3D.scale.z <=2.5) {
             gltfModel.object3D.scale.x = this.scaleFactor * this.initialScale.x;
             gltfModel.object3D.scale.y = this.scaleFactor * this.initialScale.y;
             gltfModel.object3D.scale.z = this.scaleFactor * this.initialScale.z;
         }
        else
          {
-            gltfModel.object3D.scale.x = this.data.min * 2;
-            gltfModel.object3D.scale.y = this.data.min * 2;
-            gltfModel.object3D.scale.z = this.data.min * 2;       
+            gltfModel.object3D.scale.x = this.data.min * this.data.min;
+            gltfModel.object3D.scale.y = this.data.min * this.data.min;
+            gltfModel.object3D.scale.z = this.data.min * this.data.min;       
          }
     }
 });
